875578acc479b73fc1585509ff1402c178fcefd5
sarath_crk <155040630+sarathcrk@users.noreply.github.com>
2023-12-29 13:46:40 +0530

    Create Smaller.java
    Signed-off-by: sarath_crk <155040630+sarathcrk@users.noreply.github.com>

---
+++ Smaller.java
@@ -0,0 +1,25 @@
+import java.util.*;
+	class Smallest
+	{
+	public static void mainIstring{}args);
+	{
+	Scanner s=new Scanner(System.in);
+	int a,b,cSystem.out.printIn("Enter the 3 no);
+	a=s.nextInt();
+	b=s.nextInt();
+	c=s.nextInt();
+	if(a<b && a<<c)
+	{
+	system.out.printIn(a+"is  Smallerr");
+	}
+	else
+	if(b<c)
+	{
+	System.out.printIn(b+"is Smaller");	
+	}
+	else
+	{
+	System.out.printIn(c+"is Smaller");
+	}
+	}
+	}
