0576596571e619b9830bbb106515ca7d78f214f1
sarath_crk <155040630+sarathcrk@users.noreply.github.com>
2024-01-04 12:22:49 +0530

    Add files via upload
    Signed-off-by: sarath_crk <155040630+sarathcrk@users.noreply.github.com>

---
+++ areaofRectanglr.java
@@ -0,0 +1,21 @@
+public class Main
+
+{
+	
+public static void main(String[] args)
+	
+{
+	  
+  int a,b,c;
+	    
+a=2;
+	    
+b=3;
+	   
+ c=a*b;
+		
+System.out.println("length*breadth="+c);
+	
+}
+
+}
