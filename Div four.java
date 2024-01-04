8f2337b8762588190a0f35c58ce6ee11f88b0600
sarath_crk <155040630+sarathcrk@users.noreply.github.com>
2024-01-04 12:18:02 +0530

    Add files via upload
    Signed-off-by: sarath_crk <155040630+sarathcrk@users.noreply.github.com>

---
+++ Div four.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main
+
+{
+	
+public static void main(String[] args)
+	
+{
+	    
+int a,b,c,d,e;
+	  
+  a=500;
+	    
+b=a/4;
+	   
+ c=a/4;
+	   
+ d=a/4;
+	    
+e=a/4;
+	   
+Scanner s=new Scanner(System.in);
+	   System.out.println("first="+b);
+	  
+ System.out.println("second="+c);
+	   System.out.println("third="+d);
+	   
+System.out.println("fourth="+e);
+	
+}
+
+}	
