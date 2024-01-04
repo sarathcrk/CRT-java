7b913054ed3faacea091d1295e0df247a60bbb8b
sarath_crk <155040630+sarathcrk@users.noreply.github.com>
2024-01-04 12:20:46 +0530

    Add files via upload
    Signed-off-by: sarath_crk <155040630+sarathcrk@users.noreply.github.com>

---
+++ Rectangle2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main
+
+{
+	
+public static void main(String[] args)
+	
+{
+	   
+int a,b,c;
+	    
+Scanner s=new Scanner(System.in);
+	    System.out.println("enter the length and breadth");
+	    a=s.nextInt();
+	    
+b=s.nextInt();
+	   
+ c=a*b;
+		
+System.out.println("area of rectangle="+c);
+	
+}
+
+}
