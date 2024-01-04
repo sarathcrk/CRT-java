3f8cc31bc8410c542ec462044b2fa331fda63b33
sarath_crk <155040630+sarathcrk@users.noreply.github.com>
2024-01-04 12:22:25 +0530

    Add files via upload
    Signed-off-by: sarath_crk <155040630+sarathcrk@users.noreply.github.com>

---
+++ numbigg.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Big
+{
+	public static void main(String[] args)
+	{
+	  Scanner s=new Scanner(System.in);
+	  int a,b;
+	  System.out.println("Enter two num");
+	  a=s.nextInt();
+	  b=s.nextInt();
+	  if(a>b)
+	  {
+	      System.out.println(a+"is Bigger");
+	  }
+	  else
+	  {
+	      System.out.println(b+"is Bigger");
+	  }  
+	}
+}
