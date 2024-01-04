87dc8db363aa0bb0a74253a6ba6a9f9b424e0ef9
sarath_crk <155040630+sarathcrk@users.noreply.github.com>
2024-01-04 12:26:21 +0530

    Add files via upload
    Signed-off-by: sarath_crk <155040630+sarathcrk@users.noreply.github.com>

---
+++ fabinnaccci,loop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Fibonacci
+{
+	public static void main(String[] args) 
+	{
+	    Scanner s=new Scanner(System.in);
+	    int i;
+	    for(i=1;i<=10;i++)
+	    {
+	        if(i==5)
+	    {
+	        break;
+	    }
+	    System.out.println(i);
+	    }
+	}
+}
