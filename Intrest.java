279ab6ca390ccdfd8915baddb8950942142b65c8
sarath_crk <155040630+sarathcrk@users.noreply.github.com>
2024-01-04 12:18:40 +0530

    Add files via upload
    Signed-off-by: sarath_crk <155040630+sarathcrk@users.noreply.github.com>

---
+++ Intrest.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main
+
+{
+	
+public static void main(String[] args)
+	
+{
+	  
+ Scanner s=new Scanner(System.in);
+	  
+ double p,t,r,i;
+	  
+ System.out.println("enter principle");
+	   p=s.nextDouble();
+	  
+ System.out.println("enter time");
+	   t=s.nextDouble();
+	  
+ System.out.println("enter rate");
+	   r=s.nextDouble();
+	 
+  i=p*t*r/100;
+	
+System.out.println("Hello World"); 
+	System.out.println("INTREST="+i);
+	
+}
+
+}
