89bbd7c42e5ebef53b8933c6646e462e9d92e0c9
sarath_crk <155040630+sarathcrk@users.noreply.github.com>
2024-01-04 12:17:06 +0530

    Add files via upload
    Signed-off-by: sarath_crk <155040630+sarathcrk@users.noreply.github.com>

---
+++ Average.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main
+
+{
+	
+public static void main(String args[])
+	
+{
+	
+int a,b,c,d;
+	
+Scanner s=new Scanner(System.in);
+	System.out.println("Enter two no");
+	
+a=s.nextInt();
+	
+b=s.nextInt();
+	
+c=s.nextInt();
+	
+d=(a+b+c)/3;
+	
+System.out.println("Average="+d);
+
+	}
+
+}
